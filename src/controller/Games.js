73dbec075674fbe90dd8f9f87ede15e3e64f0b48
Alexandre <alexandrefsoficial@gmail.com>
2023-02-09 20:17:41 -0300

    return added to get method

--- src/controller/Games.js
+++ src/controller/Games.js
@@ -3,9 +3,9 @@ import { db } from "../config/database.js";
 export async function getGames(req, res) {
     try {
         const games = await db.query(`SELECT * FROM games`);
-        res.send(games.rows);
+        return res.send(games.rows);
     } catch (err) {
-        res.status(500).send(err.message);
+        return res.status(500).send(err.message);
     }
 }
 
