b976962a65c4349244b467c059b3979fdfc7625c
Alexandre <alexandrefsoficial@gmail.com>
2023-02-09 20:25:31 -0300

    post customer implemented

--- src/controller/Customers.js
+++ src/controller/Customers.js
@@ -8,3 +8,17 @@ export async function getCustomers(req, res) {
         return res.status(500).send(err.message);
     }
 }
+
+export async function postCustomer(req, res) {
+    const { name, phone, cpf, birthday } = req.body;
+    try {
+        const cpfTaken = await db.query(`SELECT * FROM customers WHERE cpf = $1`,[cpf]);
+        if(cpfTaken.rows.length !== 0){
+            return res.sendStatus(409);
+        }
+        await db.query(`INSERT INTO customers (name,phone,cpf,birthday) VALUES ($1,$2,$3,$4)`,[name, phone, cpf, birthday]);
+        return res.sendStatus(201);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+}
--- src/routes/CustomersRouter.js
+++ src/routes/CustomersRouter.js
@@ -1,8 +1,8 @@
-import { getCustomers } from "../controller/Customers.js"
+import { getCustomers, postCustomer } from "../controller/Customers.js"
 import { Router } from 'express'
 
 const customersRouter = Router()
 
 customersRouter.get("/customers", getCustomers)
-
+customersRouter.post("/customers", postCustomer)
 export default customersRouter
