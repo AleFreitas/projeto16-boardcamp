5c0427fa42c27d9a6a811e51ab9f29265a75d015
Alexandre <alexandrefsoficial@gmail.com>
2023-02-10 20:22:07 -0300

    games validation implemented

--- src/routes/GamesRouter.js
+++ src/routes/GamesRouter.js
@@ -1,9 +1,11 @@
-import { getGames, postGame } from "../controller/Games.js" 
 import { Router } from 'express'
+import { getGames, postGame } from "../controller/Games.js" 
+import { validateSchema } from '../middleware/validateSchema.js'
+import { GamesSchema } from '../schema/GamesSchema.js'
 
 const gamesRouter = Router()
 
 gamesRouter.get("/games", getGames)
-gamesRouter.post("/games", postGame)
+gamesRouter.post("/games", validateSchema(GamesSchema), postGame)
 
 export default gamesRouter
---
+++ src/schema/GamesSchema.js
@@ -0,0 +1,8 @@
+import joi from 'joi'
+
+export const GamesSchema = joi.object({
+    name: joi.string().required(),
+    image: joi.string().uri(),
+    stockTotal: joi.number().min(1),
+    pricePerDay: joi.number().min(1)
+});
