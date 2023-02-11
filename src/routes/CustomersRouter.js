06a5f75a8505d7026345e45f517de551f983e623
Alexandre <alexandrefsoficial@gmail.com>
2023-02-10 21:14:18 -0300

    customer middleware validation implemented

--- src/routes/CustomersRouter.js
+++ src/routes/CustomersRouter.js
@@ -1,9 +1,11 @@
-import { getCustomers, postCustomer, getCustomer} from "../controller/Customers.js"
 import { Router } from 'express'
+import { getCustomers, postCustomer, getCustomer} from "../controller/Customers.js"
+import { validateSchema } from '../middleware/validateSchema.js'
+import { CostumerSchema } from '../schema/CustomerSchema.js'
 
 const customersRouter = Router()
 
 customersRouter.get("/customers", getCustomers)
 customersRouter.get("/customers/:id", getCustomer)
-customersRouter.post("/customers", postCustomer)
+customersRouter.post("/customers", validateSchema(CostumerSchema),postCustomer)
 export default customersRouter
---
+++ src/schema/CustomerSchema.js
@@ -0,0 +1,8 @@
+import joi from 'joi'
+
+export const CostumerSchema = joi.object({
+    cpf: joi.string().regex(/^\d+$/).length(11),
+    phone: joi.string().regex(/^[0-9]+$/).min(10).max(11),
+    name: joi.string().required(1),
+    birthday: joi.date()
+});
