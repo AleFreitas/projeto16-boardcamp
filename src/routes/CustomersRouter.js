1c82b8a8b8c4fbf756153ce82ca3f747bbed7294
Alexandre <alexandrefsoficial@gmail.com>
2023-02-10 21:43:41 -0300

    CustomerSchema name fixed

--- src/routes/CustomersRouter.js
+++ src/routes/CustomersRouter.js
@@ -1,11 +1,11 @@
 import { Router } from 'express'
 import { getCustomers, postCustomer, getCustomer} from "../controller/Customers.js"
 import { validateSchema } from '../middleware/validateSchema.js'
-import { CostumerSchema } from '../schema/CustomerSchema.js'
+import { CustomerSchema } from '../schema/CustomerSchema.js'
 
 const customersRouter = Router()
 
 customersRouter.get("/customers", getCustomers)
 customersRouter.get("/customers/:id", getCustomer)
-customersRouter.post("/customers", validateSchema(CostumerSchema),postCustomer)
+customersRouter.post("/customers", validateSchema(CustomerSchema),postCustomer)
 export default customersRouter
